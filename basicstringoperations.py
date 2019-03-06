2496e0b73762e806f3bf7fea36a7534894caf3bd
Daniel López Lacalle <expilu@gmail.com>
2019-03-06 12:25:29 +0000

    Create basicstringoperations.py

---
+++ basicstringoperations.py
@@ -0,0 +1,36 @@
+astring = "Hello world!"
+print("single quotes are ' '")
+
+print(len(astring))
+
+astring = "Hello world!"
+print(astring.index("o"))
+
+astring = "Hello world!"
+print(astring.count("l"))
+
+astring = "Hello world!"
+print(astring[3:7])
+
+astring = "Hello world!"
+print(astring[3:7:2])
+
+astring = "Hello world!"
+print(astring[3:7])
+print(astring[3:7:1])
+
+astring = "Hello world!"
+print(astring.upper())
+print(astring.lower())
+
+astring = "Hello world!"
+print(astring.startswith("Hello"))
+print(astring.endswith("asdfasdfasdf"))
+
+astring = "Hello world!"
+afewwords = astring.split(" ")
+print(afewwords)
+
+
+# Exercise
+# Try to fix the code to print out the correct information by changing the string.
